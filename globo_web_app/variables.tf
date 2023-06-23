0e55932a654e87e1a21420b3471dadb8e4672a86
Erickson <erickson.santos@caylent.com>
2023-06-24 01:36:39 +0800

    M6

--- globo_web_app/variables.tf
+++ globo_web_app/variables.tf
@@ -1,15 +1,3 @@
-variable "aws_access_key" {
-  type        = string
-  sensitive   = true
-  description = "AWS Access Key"
-}
-
-variable "aws_secret_key" {
-  type        = string
-  sensitive   = true
-  description = "AWS Secret Key"
-}
-
 variable "aws_region" {
   type        = string
   description = "AWS region to use for resources"
